af476bfc0a5deadfbed8d0b152cebaf49a187d62
Segowax <kosmicznybandyta@gmail.com>
2025-02-07 23:12:53 +0100

    try catch on program.cs

--- API/Program.cs
+++ API/Program.cs
@@ -1,37 +1,44 @@
 using Azure.Identity;
 
-var builder = WebApplication.CreateBuilder(args);
+try
+{
+    var builder = WebApplication.CreateBuilder(args);
 
 #if DEBUG
-var conn = builder.Configuration.GetSection("AzureAppConfigurationConnectionString");
-builder.Configuration.AddAzureAppConfiguration(
-    cfg => cfg.Connect(conn.Value));
+    var conn = builder.Configuration.GetSection("AzureAppConfigurationConnectionString");
+    builder.Configuration.AddAzureAppConfiguration(
+        cfg => cfg.Connect(conn.Value));
 #else
 builder.Configuration.AddAzureAppConfiguration(options =>
     options.Connect(
         new Uri(builder.Configuration["AppConfig:Endpoint"] ?? throw new ArgumentNullException("AppConfig:Endpoint")),
         new ManagedIdentityCredential()));
 #endif
-builder.Services.AddAzureAppConfiguration();
+    builder.Services.AddAzureAppConfiguration();
 
-// Add Application Insights telemetry collection.
-builder.Services.AddApplicationInsightsTelemetry();
+    // Add Application Insights telemetry collection.
+    builder.Services.AddApplicationInsightsTelemetry();
 
-builder.Services.AddControllers();
+    builder.Services.AddControllers();
 
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+    // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+    builder.Services.AddEndpointsApiExplorer();
+    builder.Services.AddSwaggerGen();
 
-var app = builder.Build();
+    var app = builder.Build();
 
-app.UseSwagger();
-app.UseSwaggerUI();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 
-app.UseHttpsRedirection();
+    app.UseHttpsRedirection();
 
-app.UseAuthorization();
+    app.UseAuthorization();
 
-app.MapControllers();
+    app.MapControllers();
 
-app.Run();
+    app.Run();
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.ToString());
+}
