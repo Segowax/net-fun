f82c8ba4c97a55b86fe287902afe981812f78ea1
Segowax <kosmicznybandyta@gmail.com>
2025-02-07 22:37:06 +0100

    App Configuration set

--- API/Program.cs
+++ API/Program.cs
@@ -1,8 +1,23 @@
+using Azure.Core;
+using Azure.Identity;
+using Microsoft.Extensions.Configuration;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+#if DEBUG
+var conn = builder.Configuration.GetSection("AzureAppConfigurationConnectionString");
+builder.Configuration.AddAzureAppConfiguration(
+    cfg => cfg.Connect(conn.Value));
+#else
+builder.Configuration.AddAzureAppConfiguration(options =>
+    options.Connect(
+        new Uri(builder.Configuration["AppConfig:Endpoint"] ?? throw new ArgumentNullException("AppConfig:Endpoint")),
+        new ManagedIdentityCredential()));
+#endif
 
 builder.Services.AddControllers();
+builder.Services.AddAzureAppConfiguration();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
