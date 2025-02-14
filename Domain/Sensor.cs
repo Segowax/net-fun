256086fddc56d335f1a7cb244e282ed1dd618e78
Segowax <kosmicznybandyta@gmail.com>
2025-02-14 07:55:31 +0100

    Domain project introduced. Sensor entity added.

---
+++ Domain/Sensor.cs
@@ -0,0 +1,11 @@
+﻿namespace Domain
+{
+    public class Sensor
+    {
+        public int PropertyId { get; set; }
+        public required string Name { get; set; }
+        public required object Value { get; set; }
+        public DateTime MeasurementTime { get; set; }
+        public DateTime EnqueuedTime { get; set; }
+    }
+}
