0e3a55c70f84c2e42bbd94495e0af0735501a0a8
Segowax <kosmicznybandyta@gmail.com>
2025-03-16 15:44:50 +0100

    1wire initialization

---
+++ SensorHub/Configurations/DS18B20/DS18B20.h
@@ -0,0 +1,18 @@
+/*
+ * DS18B20.h
+ *
+ *  Created on: 16 mar 2025
+ *      Author: KosmicznyBandyta
+ */
+
+#ifndef CONFIGURATIONS_DS18B20_DS18B20_H_
+#define CONFIGURATIONS_DS18B20_DS18B20_H_
+
+// ROM function commands
+#define READ_ROM 0x33
+#define MATCH_ROM 0x55
+#define SKIP_ROM 0xCC
+#define SEARCH_ROM 0xf0
+#define ALARM_SEARCH 0xEC
+
+#endif /* CONFIGURATIONS_DS18B20_DS18B20_H_ */
--- SensorHub/main.c
+++ SensorHub/main.c
@@ -5,43 +5,48 @@
  *      Author: KosmicznyBandyta
  */
 
-#include <avr/pgmspace.h>
-#include <avr/interrupt.h>
 #include <util/delay.h>
 
-#include "Configurations/timer0.h"
 #include "Configurations/rs232.h"
-#include "Configurations/inputs.h"
-#include "Configurations/buffer.h"
+#include "Configurations/DS18B20/1wire.h"
+#include "Configurations/DS18B20/DS18B20.h"
 
 int main() {
-	USART_init(__UBRR);
-	buttons_init();
-	circular_buffer_init(&buffer_for_timer0);
-	timer0_init();
+	uint8_t rom[8];
+	uint8_t byte;
+	uint8_t bit;
+	int8_t i;
+	int8_t j;
 
-	static bool door_state = false;
+	if (!onewire_reset_presence_procedure()) {
+		onewire_byte_write(READ_ROM);
+		for (i = 0; i < 8; i++) {
+			byte = onewire_byte_read();
+			rom[i] = byte;
+		}
+	}
+	if (crc8(rom, 8)) {
+		// error
+	}
+
+	USART_init(__UBRR);
 
 	_delay_ms(10);
 
-	sei();
+	for (i = 7; i >= 0; i--) {
+		byte = rom[i];
+		for (j = 7; j >= 0; j--) {
+			bit = (byte >> j) & 1;
+			if (bit)
+				USART_Transmit('1');
+			else
+				USART_Transmit('0');
+		}
+	}
+
+	USART_Transmit('\n');
 
 	while (1) {
-		circular_buffer_pop(&buffer_for_timer0);
-		circular_buffer_pop(&buffer_for_door);
 
-
-		// move it to int0 interrupt?
-		bool current_door_state = is_closed(KEY_PORT, KEY_PIN_1);
-		if (door_state != current_door_state) {
-			door_state = current_door_state;
-			if (door_state) {
-				circular_buffer_push(&buffer_for_door,
-						"{\"SensorId\":\"nax_open_closed_sensor_1\",\"Name\":\"Inside Lock\",\"Value\":\"Closed\"}@");
-			} else {
-				circular_buffer_push(&buffer_for_door,
-						"{\"SensorId\":\"nax_open_closed_sensor_1\",\"Name\":\"Inside Lock\",\"Value\":\"Open\"}@");
-			}
-		}
 	}
 }
